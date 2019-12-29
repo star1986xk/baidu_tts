592c7186c4deb59ba7d00cf119e9f09fb81002d5
lijian <173013905@qq.com>
2019-12-30 01:49:23 +0800

    12.30

--- pytts.py
+++ pytts.py
@@ -3,13 +3,25 @@
 
 import pyttsx3
 
-f = open("1.txt",'r')
-line = f.readline()
+with open("2.txt",'r',encoding='utf-8') as f:
+    line = f.read()
+list_line = line.split('\n')
+
 engine = pyttsx3.init()
-# while line:
-#     line = f.readline()
-#     print(line, end = '')
-#     engine.say(line)
-engine.say('message')
-engine.runAndWait()
-f.close()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-20)
+for li in list_line:
+    line_list = li.split(',')
+    print(line_list)
+    engine.say(line_list[0])
+    engine.say(list(line_list[0]))
+    engine.say(line_list[1])
+
+    engine.say(line_list[0])
+    engine.say(list(line_list[0]))
+    engine.say(line_list[1])
+
+    engine.say(line_list[0])
+    engine.say(list(line_list[0]))
+    engine.say(line_list[1])
+    engine.runAndWait()
--- sounddevice_test.py
+++ sounddevice_test.py
@@ -3,12 +3,11 @@
 import numpy as np
 import sounddevice as sd
 from scipy.io import wavfile
-from scipy.io import wavfile
 
 
 # print(sd.query_devices())
 fs = 44100 # Hz
 length = 10 # s
-sd.default.device[0] = 8
+# sd.default.device[0] = 8
 recording = sd.rec(frames=fs * length, samplerate=fs, blocking=True, channels=1)
 wavfile.write('recording.wav', fs, recording)
